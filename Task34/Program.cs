a4c0a6c2ab33b4f0577e2a14aa49fe9582706226
Dmitriy <nyx1889gmail.com>
2022-07-13 22:37:29 +0300

    added EvenNumbers

---
+++ Task34/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int [] array = new int [10];
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+Console.WriteLine(string.Join(", ", array));
+
+int EvenNumbers (int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count = count + 1;
+        }    
+    }
+    return count;
+}
+Console.WriteLine(EvenNumbers(array));
