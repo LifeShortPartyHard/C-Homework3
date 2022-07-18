c476a31bc7ee2f995f45641b95e7c9d584a27324
Dmitriy <nyx1889gmail.com>
2022-07-18 19:57:56 +0300

    added amount of positive numbers

---
+++ Task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество чисел:");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] array  = new int [M];
+
+int amountOfPositiveNumbers (int [] arr)
+{    
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if (arr[i] > 0)
+        {
+            result++;
+        }
+    }    
+    return result; 
+}
+
+Console.WriteLine("Количество положительных чисел: " + amountOfPositiveNumbers(array));
+
