4d674fb40fe3945c5bdecc428e8f89e966d5d757
Dmitriy <nyx1889gmail.com>
2022-07-19 22:27:54 +0300

    added random array

---
+++ Task47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double [,] randomArray(double [,] array)
+{ 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Convert.ToDouble(new Random().Next(-100,100)/10.0);
+        }
+    }
+    return array;
+}
+
+void printArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 5;
+int n = 4;
+double [,] array = new double [m,n];
+printArray(randomArray(array));
