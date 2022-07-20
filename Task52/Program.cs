986d80d34aa48ba1c2c5ac139c264d4e19f93c80
Dmitriy <nyx1889gmail.com>
2022-07-20 23:22:34 +0300

    added average

---
+++ Task52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] getArray (int amountOfRows, int amountOfColumns)
+{
+    int [,] array = new int [amountOfRows,amountOfColumns];
+    for (int i = 0; i < amountOfRows; i++)
+    {
+        for (int j = 0; j < amountOfColumns; j++)
+        {
+            array [i,j] = new Random().Next(-10,10);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void average (int [,] array)
+{
+    double result = 0;
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            result = result + array [j,i];            
+        }
+        Console.Write(result / array.GetLength(0) + "; ");
+        result = 0;
+    }
+}
+
+int rows = 4;
+int columns = 4;
+int [,] array = getArray(rows, columns);
+printArray(array);
+average(array);
