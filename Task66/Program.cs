e0dccf94d6a5123042512cb566f80aba77fe7d25
Dmitriy <nyx1889gmail.com>
2022-07-26 23:24:01 +0300

    added sum of natural numbers

---
+++ Task66/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumOfNaturalNumbers( int M, int N){
+    if (M <= 0) M = 1;
+    if (M == N) return N;
+    int sum = M;
+    return sum += SumOfNaturalNumbers(M += 1, N);
+}
+
+Console.WriteLine(SumOfNaturalNumbers(-5, 20));
