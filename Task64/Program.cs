c46051016e52c87f28e595add6408438a06e8930
Dmitriy <nyx1889gmail.com>
2022-07-26 22:35:43 +0300

    added natural numbers

---
+++ Task64/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string NaturalNumbers( int M, int N){
+    if (M <= 0) M = 1;
+    return M == N ? N.ToString(): M.ToString() + " " + NaturalNumbers(M += 1, N);
+}
+
+Console.WriteLine(NaturalNumbers(5, 20));
